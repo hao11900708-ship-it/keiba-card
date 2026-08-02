5c8f832f7945331ddb4cad914edf4af1ec698db6
hao11900708-ship-it <hao11900708@gmail.com>
2026-08-02 23:40:34 +0900

    update service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+
+const CACHE = 'keiba-202608022340';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
