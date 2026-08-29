c7bc94c337b4089d46947e4448f2864223deceb8
hao11900708-ship-it <hao11900708@gmail.com>
2026-08-29 16:07:50 +0900

    update service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'keiba-202608291552';
+const CACHE = 'keiba-202608291607';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
